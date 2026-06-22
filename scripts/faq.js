6fc8691d161964ddccbeeaef1b8b08a0fecf8a46
nofugazidev <oyebisimubarak4@gmail.com>
2026-06-22 16:29:35 +0100

    ui: implemented the desktop ui for faq section

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,19 +1,20 @@
-const toggleArrows = document.querySelectorAll(".faq-box i");
-const answerBoxes = document.querySelectorAll(".answer")
-
+const toggleArrows = document.querySelectorAll(".faq-box .icon");
+const answerBoxes = document.querySelectorAll(".answer");
+const faBoxes = document.querySelectorAll("#faqBox");
 
 toggleArrows.forEach((arrow, index) => {
-    arrow.addEventListener('click', ()=> {
-        const matchingAnswer = answerBoxes[index];
+  arrow.addEventListener("click", () => {
+    const matchingAnswer = answerBoxes[index];
+    const faBox = faBoxes[index];
 
-        matchingAnswer.classList.toggle('is-open')
-        
-        if(matchingAnswer.classList.contains('is-open')){
-            arrow.classList.remove("fa-arrow-down")
-            arrow.classList.add("fa-arrow-up")
-        } else {
-            arrow.classList.remove("fa-arrow-up")
-            arrow.classList.add("fa-arrow-down")
-        }
-    })
-})
+    matchingAnswer.classList.toggle("is-open");
+
+    if (matchingAnswer.classList.contains("is-open")) {
+      arrow.textContent = "-";
+      faBox.style.backgroundColor = "#ffdd81";
+    } else {
+      arrow.textContent = "+";
+      faBox.style.backgroundColor = "#f7f7f7";
+    }
+  });
+});
